4681764695fe18d3d55dd756970bc074d7a8ce21
Nicolas Hovart <kola.hov@gmail.com>
2017-12-27 01:17:52 +0100

    Add gem now ui rails

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'pry-byebug'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'rack-mini-profiler'
+gem 'now-ui-kit-rails'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require rails-ujs
-//= require_tree .
+
+-> pourquoi on a rien dans lib/assets ..
 
 // Appelle la librairie Now-UI Kit
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
